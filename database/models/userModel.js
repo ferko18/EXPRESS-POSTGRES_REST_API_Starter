cd62a130f0e364d88d3a7a6d2c92ee4bdb2c77cc
Firealem Erko <firealemg@yahoo.com>
2019-06-14 20:20:00 -0400

    user crud done

--- database/models/userModel.js
+++ database/models/userModel.js
@@ -1,27 +1,54 @@
-const pool = require("../connection.js")
+const pool = require("../connection.js");
 
-//get all users 
-async function getAllUsers()
-{
-    const res = await pool.query('select * from users')
-    return res.rows;
+//get all users
+async function getAllUsers() {
+  const res = await pool.query("select * from users");
+  return res.rows;
 }
 
-//get user by id 
-async function getOneUser(id){
-    const text = 'select * from users where user_id=$1'
-    values = [id]
-    const res= await pool.query(text, values)
-    return res.rows
+//get user by id
+async function getOneUser(id) {
+  //parametrized query
+  // const text = 'select * from users where user_id=$1'
+  // values = [id]
+  //const res = await pool.query(text, values);
+
+  //we can also use prepared statment
+  const query = {
+    name: "getOneUser",
+    text: "select * from users where user_id=$1",
+    values: [id]
+  };
+  const res = await pool.query(query);
+  return res.rows;
 }
 
-//add user 
+//add user , note that the returing * statment returns the inserted record
 
-async function addUser(first_name, last_name,email, password){
-    const text = 'insert into users(first_name, last_name, email, password) values ($1,$2, $3, $4) returning user_id'
-    const values=[first_name,last_name,email, password]
-    const add = await pool.query(text, values)
-    return add.rows
+async function addUser(first_name, last_name, email, password) {
+  const text =
+    "insert into users(first_name, last_name, email, password) values ($1,$2, $3, $4) returning *";
+  const values = [first_name, last_name, email, password];
+  const add = await pool.query(text, values);
+  return add.rows;
 }
 
-module.exports = {getAllUsers, getOneUser, addUser}
+//update user 
+
+async function updateUser(id, first_name, last_name, email, password) {
+    const text = "update users set first_name=$1, last_name=$2, email=$3, password=$4 where user_id=$5 returning *"
+    const values = [first_name, last_name, email, password, id]
+    const updated = await pool.query(text, values)
+    return updated.rows
+}
+
+//delete user 
+
+async function deleteUser(id) {
+    const text = "delete from users where user_id=$1 returning user_id"
+     const values = [id]
+    const deleted = await pool.query(text, values)
+    return deleted.rows
+}
+
+module.exports = { getAllUsers, getOneUser, addUser , updateUser, deleteUser};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 const db = require("../database/models/userModel");
 
-//get all users 
+//get all users
 router.get("/", async (req, res) => {
   try {
     const users = await db.getAllUsers();
@@ -12,7 +12,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-//get user by id 
+//get user by id
 
 router.get("/:id", async (req, res) => {
   try {
@@ -29,17 +29,57 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// add user 
+// add user
 
 router.post("/", async (req, res) => {
-    try {
-     const {first_name, last_name, email, password}=req.body
-      const newuser = await db.addUser(first_name,last_name, email, password);
+  try {
+    const { first_name, last_name, email, password } = req.body;
+    if (!first_name || !last_name || !email || !password) {
+      res.status(400).json({ error: " provide all fields." });
+    } else {
+      const newuser = await db.addUser(first_name, last_name, email, password);
       res.status(200).json(newuser);
-    } catch (err) {
-      res.status(500).json({ message: "could add user to the database" });
     }
-  });
+  } catch (err) {
+    res.status(500).json({ message: "could not add user to the database" });
+  }
+});
 
+//update user
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { first_name, last_name, email, password } = req.body;
+    const user = await db.updateUser(
+      id,
+      first_name,
+      last_name,
+      email,
+      password
+    );
+    if (user.length === 0) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "can't update user" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await db.deleteUser(id);
+    if (user.length === 0) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "can't delete user" });
+  }
+});
 
 module.exports = router;
